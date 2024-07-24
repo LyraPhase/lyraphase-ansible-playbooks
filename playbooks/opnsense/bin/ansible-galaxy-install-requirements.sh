2e70c8b2dd724baade85485f51bd5110f723026c
James Cuzella <james.cuzella@lyraphase.com>
2024-07-23 18:39:14 -0600

    OPNsense: Add Ansible Galaxy Collection requirements
    Change-Id: Iefa8ead9685a01a0e591df588c5d61354faa2e7d

---
+++ playbooks/opnsense/bin/ansible-galaxy-install-requirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT=$(basename "$0")
+PLAYBOOK_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
+REPO_BASE=$( cd "${PLAYBOOK_BASE}/../../" && pwd )
+
+ansible-galaxy collection install -r "${REPO_BASE}/requirements.txt"
