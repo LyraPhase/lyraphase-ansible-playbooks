bddac3cdc5aa3fef397d950f6b6d8d979c7677b1
James Cuzella <james.cuzella@lyraphase.com>
2024-09-20 13:34:29 -0600

    OPNsense: install requirements.yml roles too
    Change-Id: Ia9ae0a785c70b1f3ff2aaf2da75894c999f7f423

--- playbooks/opnsense/bin/ansible-galaxy-install-requirements.sh
+++ playbooks/opnsense/bin/ansible-galaxy-install-requirements.sh
@@ -4,4 +4,5 @@ SCRIPT=$(basename "$0")
 PLAYBOOK_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 REPO_BASE=$( cd "${PLAYBOOK_BASE}/../../" && pwd )
 
-ansible-galaxy collection install -r "${REPO_BASE}/requirements.txt"
+ansible-galaxy collection install -r "${REPO_BASE}/requirements.yml"
+ansible-galaxy role install -r "${REPO_BASE}/requirements.yml"
