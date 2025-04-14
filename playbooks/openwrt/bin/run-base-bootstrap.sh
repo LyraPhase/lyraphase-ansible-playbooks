ca9ceb943ec309dfbe4dc9efaebedd8f68ffd21e
James Cuzella <james.cuzella@lyraphase.com>
2025-04-14 00:07:36 -0600

    openwrt-base: bootstrap: Refactor REPO_BASE -> PLAYBOOK_BASE for proper repo & playbook paths
    Change-Id: I058f3846a4a17ab1c6aa161874e8fb9709404ecc

--- playbooks/openwrt/bin/run-base-bootstrap.sh
+++ playbooks/openwrt/bin/run-base-bootstrap.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 SCRIPT=$(basename "$0")
-REPO_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
+PLAYBOOK_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )
+REPO_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" && pwd )
+ROLE_NAME=$(basename "$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)" )
+source "${REPO_BASE}/bin/setup-ansible-vault.sh"
+
+echo "${ROLE_NAME}: ${SCRIPT}"
 
 # If using docker-machine + VirtualBox...
 # Hack for forcing mdns / Bonjour / Zeroconf DNS resolution to be done on the Host machine running the VM
@@ -13,4 +18,4 @@ if [ -n "${DOCKER_MACHINE_NAME}" ]; then
 fi
 
 # First run must SSH in as root with asked password (we assume you have set a password first)
-ansible-playbook -i "${REPO_BASE}"/inventory/hosts "${REPO_BASE}"/base.yml -vv --diff --user=root --ask-pass
+ansible-playbook -i "${PLAYBOOK_BASE}"/inventory/hosts "${PLAYBOOK_BASE}"/base.yml -vv --diff --user=root --ask-pass
