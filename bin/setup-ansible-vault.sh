5405394bc39bd6f7617cc06a81c8d660e167be1a
James Cuzella <james.cuzella@lyraphase.com>
2025-05-13 17:24:12 -0600

    dev: setup-ansible-env.sh: Add zsh support with readline LPASS_USERNAME prompt
    Change-Id: I714e6da3a04bf8b8a9f6ae719d4f89a26a0343b7

--- bin/setup-ansible-vault.sh
+++ bin/setup-ansible-vault.sh
@@ -6,6 +6,13 @@ ANSIBLE_VAULT_PASSWORD_FILE=$(which lastpass-ansible)
 export ANSIBLE_VAULT_PASSWORD_FILE
 
 if ! lpass status 1>/dev/null 2>&1 ; then
-  [ -n "$LPASS_USERNAME" ] || read -r -p 'Enter LastPass login email: ' LPASS_USERNAME
+  _this_shell=$(ps -p $$ | awk '$1 != "PID" {print $4}')
+  if [[ "$_this_shell" == 'zsh' ]]; then
+    _read_cmd="LPASS_USERNAME=\$(zsh -c 'vared -c -p '\''Enter LastPass login email: '\'' LPASS_USERNAME ; printf %s \"\$LPASS_USERNAME\"')"
+    echo "$_read_cmd"
+  else
+    _read_cmd="read -r -p 'Enter LastPass login email: ' LPASS_USERNAME"
+  fi
+  [ -n "$LPASS_USERNAME" ] || eval "$_read_cmd"
   lpass login "$LPASS_USERNAME"
 fi
