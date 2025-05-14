2117feb274804437e07fbfa9d9c73e5f0f59bb51
James Cuzella <james.cuzella@lyraphase.com>
2025-05-14 10:53:48 -0600

    openwrt-base: run-base-bootstrap: Tag & run only bootstrap tasks
    Change-Id: I1056bcfe7085c9f06c60d40779c909415040e96a

--- playbooks/openwrt/bin/run-base-bootstrap.sh
+++ playbooks/openwrt/bin/run-base-bootstrap.sh
@@ -18,4 +18,4 @@ if [ -n "${DOCKER_MACHINE_NAME}" ]; then
 fi
 
 # First run must SSH in as root with asked password (we assume you have set a password first)
-ansible-playbook -i "${PLAYBOOK_BASE}"/inventory/hosts "${PLAYBOOK_BASE}"/base.yml --extra-vars 'ansible_bootstrap=true' -vv --diff --user=root --ask-pass "$@"
+ansible-playbook -i "${PLAYBOOK_BASE}"/inventory/hosts "${PLAYBOOK_BASE}"/base.yml --extra-vars 'ansible_bootstrap=true' -vv --diff --user=root --ask-pass --tags=openwrt_bootstrap "$@"
