efa3feb995ed4892e7948506ffa1dbc263c5dba2
James Cuzella <james.cuzella@lyraphase.com>
2025-09-03 12:24:55 -0600

    dev: ee: Add zsh, omz + p10k to final image

---
+++ ee/profile.d/p10k-centos.zsh
@@ -0,0 +1,6 @@
+# Closest match to: #bc1142  = #a14f8c
+# Closest match to: #f0a91f  = #e6cd69
+typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=003 # #e6cd69 yellow
+typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=005 # #a074c4 magenta
+# Custom icon.
+typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
