97e069ba61a954a4bc5443cfd514431ac8991209
James Cuzella <james.cuzella@lyraphase.com>
2025-10-13 07:58:39 -0600

    dev: ee: Set p10k CentOS colors via delta-e closest match & force init

--- ee/profile.d/p10k-centos.zsh
+++ ee/profile.d/p10k-centos.zsh
@@ -1,6 +1,12 @@
-# Closest match to: #bc1142  = #a14f8c
-# Closest match to: #f0a91f  = #e6cd69
-typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=003 # #e6cd69 yellow
-typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=005 # #a074c4 magenta
+_p9k__force_must_init=1
+export CENTOS_POWERLEVEL9K_CONFIG_FILE=${CENTOS_POWERLEVEL9K_CONFIG_FILE:-/etc/profile.d/p10k-centos.zsh}
+# Closest match to: #bc1142  = #d7005f
+# Closest match to: #a14f8c  = #af5f87
+# Closest match to: #f0a91f  = #ffaf00
+typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=214 # #ffaf00 orange1
+typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=161 # #d7005f magenta5
+typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=132 # #af5f87 magenta4
 # Custom icon.
 typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
+
+export POWERLEVEL9K_OS_ICON_FOREGROUND POWERLEVEL9K_OS_ICON_BACKGROUND POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION
