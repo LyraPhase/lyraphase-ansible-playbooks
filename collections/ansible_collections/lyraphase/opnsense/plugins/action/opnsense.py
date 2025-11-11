e093b8a718995b3b1ee8aa98ca7e00bb37295442
James Cuzella <james.cuzella@lyraphase.com>
2025-11-11 15:53:56 -0700

    lyraphase.opnsense: Add initial action plugin shim + symlinks

---
+++ collections/ansible_collections/lyraphase/opnsense/plugins/action/opnsense.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+#
+# -*- coding: utf-8 -*-
+# Copyright 2025 LyraPhase LLC
+# Copyright 2025 James Cuzella (@trinitronx)
+#
+# This file is part of LyraPhase OPNsense Collection
+#
+# LyraPhase OPNsense Collection is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# LyraPhase OPNsense Collection is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with LyraPhase OPNsense Collection.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+from ansible.module_utils.common.text.converters import to_text
+from ansible.utils.display import Display
+from ansible_collections.ansible.netcommon.plugins.action.network import (
+    ActionModule as ActionNetworkModule,
+)
+
+
+display = Display()
+
+
+class ActionModule(ActionNetworkModule):
+    def run(self, tmp=None, task_vars=None):
+        # BEGIN DEBUG INSTRUMENTATION
+        # import debugpy
+
+        # debugpy.listen(5678)
+        # debugpy.wait_for_client()
+        # debugpy.breakpoint()
+        # END DEBUG INSTRUMENTATION
+        del tmp  # tmp no longer has any effect
+
+        module_name = self._task.action.split(".")[-1]
+        # TODO: remove config modules
+        self._config_module = True if module_name in ["asa_config", "config"] else False
+        persistent_connection = self._play_context.connection.split(".")[-1]
+        warnings = []
+
+        display.vvvv("config module: %s" % to_text(self._config_module, errors="surrogate_then_replace"))
+        display.vvvv("persistent_connection: %s" % to_text(persistent_connection, errors="surrogate_then_replace"))
+
+        if persistent_connection not in ("network_cli"):
+            return {
+                "failed": True,
+                "msg": "Connection type %s is not valid for this module" % self._play_context.connection,
+            }
+
+        result = super(ActionModule, self).run(task_vars=task_vars)
+        if warnings:
+            if "warnings" in result:
+                result["warnings"].extend(warnings)
+            else:
+                result["warnings"] = warnings
+        return result
